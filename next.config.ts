99b79de16b13485c18315029b21166cb9e0f49e5
Luis Alberto Tadeo Lopez <TU-CORREO@gmail.com>
2026-05-01 19:20:17 -0400

    add app icons

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,22 @@
 import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
+
+// Inicializar plugin PWA
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [],
+  reactStrictMode: true,
+
+  // Opcional pero recomendado para producción
+  experimental: {
+    typedRoutes: true,
   },
 };
 
-export default nextConfig;
+// Export final con PWA activado
+export default withPWA(nextConfig);
